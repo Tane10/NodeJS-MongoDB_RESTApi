2956e1c6fb592fe81ba68ef2166eb1ab51bc1eaa
Tillie Smith <tillie@tillies-mbp.home>
2019-02-25 23:41:58 +0000

    added test data and now using mongo db

---
+++ ItemsDummyDataJSON.js
@@ -0,0 +1,22 @@
+db.items.insert([
+    {
+        itemName: 'Front Wheel',
+        itemDescription: 'Front Wheel of mtb',
+        quality: 'A',   
+    },
+    {
+        itemName: 'Back Wheel',
+        itemDescription: 'Back Wheel of mtb',
+        quality: 'A',   
+    },
+    {
+        itemName: 'Front BreakPad',
+        itemDescription: 'Front BreakPad of mtb',
+        quality: 'A',   
+    },
+    {
+        itemName: 'Back BreakPad',
+        itemDescription: 'Back BreakPad of mtb',
+        quality: 'A',   
+    }
+])
--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 
 const app = express();
-
+const itemsRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+itemsRouter.route('/items')
+    .get((req, res) => {
+        const response = {hello: "this is it api"};
+        res.json(response);
+    });
+
+app.use('/api', itemsRouter);
 
 app.get("/", (req, res) => {
   res.send("welcome");
