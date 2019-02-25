34633275fb78d50104add2be969133136d87c581
Tillie Smith <tillie@tillies-mbp.home>
2019-02-25 19:34:14 +0000

    done a put and very basic root api call.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to mongoDB api");
+});
+
+app.listen(port, () => {
+  console.log(`running on ${port}`);
+});
