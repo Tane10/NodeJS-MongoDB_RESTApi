a806fcb205e46a0e8c0e59bfbea252ce5ba5ba33
Tillie Smith <tillie@tillies-mbp.home>
2019-02-25 23:22:19 +0000

    have a basic set up working

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to mongoDB api");
+  res.send("welcome");
 });
 
 app.listen(port, () => {
-  console.log(`running on ${port}`);
-});
+    console.log(`running on port ${port}`)
+})
