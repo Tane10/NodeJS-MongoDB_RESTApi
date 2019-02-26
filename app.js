d72a42536e107602aa687d518edd46ce85f19c4e
josh <tane2@hotmail.co.uk>
2019-02-26 12:41:36 +0000

    can search items using their name and search item using the ID

--- app.js
+++ app.js
@@ -8,21 +8,45 @@ const itemsRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Items = require("./models/itemModel");
 
+
+// Get items from mongo DB 
 itemsRouter.route("/items").get((req, res) => {
-  Items.find((err, items) => {
-    if (err) {
-      return res.send(err);
+    //filter search items using the items name in the urls
+    const  query = {};
+    if(req.query.itemName){
+        query.itemName = req.query.itemName
     }
-    return res.json(items);
-  });
+    Items.find(query, (err, items) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(items);
+    });
 });
 
+
+// Get items from mongo DB 
+itemsRouter.route("/items/:itemId").get((req, res) => {
+    //find idem by id 
+    Items.findById(req.params.itemId, (err, items) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(items);
+    });
+});
+
+// route to get to API
 app.use("/api", itemsRouter);
 
+
+// Rooot Route
 app.get("/", (req, res) => {
   res.send("welcome");
 });
 
+
+//Port to listen on
 app.listen(port, () => {
   console.log(`running on port ${port}`);
 });
